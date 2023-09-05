aaa115b1f55ceca8195eea29d3560069b553dadd
Charles Chavis <chavisca@gmail.com>
2023-09-05 03:47:31 -0400

    all models updated with field definitions and index updated with relationships

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,43 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+        isDecimal: {
+          args: [1,2],
+          msg: 'Price must be a valid decimal with up to two decimal places.'
+        }
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isInt: {
+          msg: 'Stock must be an integer.'
+        }
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      }
+    },
   },
   {
     sequelize,
