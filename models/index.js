fe2d891f8edeb6976469837616f3b7b420830584
Charles Chavis <chavisca@gmail.com>
2023-09-19 10:59:04 -0400

    fixed index.JS relationship issue to correct delete routes, added screenshot JPG

--- models/index.js
+++ models/index.js
@@ -19,14 +19,14 @@ Category.hasMany(Product, {
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: 'ProductTag',
+  through: ProductTag,
   foreignKey: 'product_id',
   // onDelete: 'CASCADE',
   hooks: true,
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: 'ProductTag',
+  through: ProductTag,
   foreignKey: 'tag_id',
   // onDelete: 'CASCADE',
   hooks: true,
